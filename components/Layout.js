25d0715089c807fd5c5b4d7bf7e01086c88704d1
Wenjie Zhou <128197156+AlexandriaSea@users.noreply.github.com>
2024-10-29 00:50:28 -0400

    create layout template

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+
+// Define a functional component Layout that accepts props
+const Layout = (props) => (
+    <Fragment>
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <title>{props.pageTitle}</title>
+        </Head>
+        {props.children}
+    </Fragment>
+);
+
+export default Layout;
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+// Extend the default Document class to customize the HTML document structure
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+
+                <Head>
+                    <meta charSet="utf-8" />
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                        crossOrigin="anonymous"
+                    />
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
